a6502f05f2690728ae92e7855311eec8ef2ed0bf
raicg <raicg2@gmail.com>
2017-11-05 20:20:00 +0000

    Add files via upload
    Deleted the threads created individually to send the messages to the clients and created a list of all sockets connections created and send the message in the main using a loop, created a new thread to receive commands in the server.
    Fixed some bugs and now its working great without any bugs !!!

--- chat-server.py
+++ chat-server.py
@@ -2,50 +2,54 @@ from socket import *
 from threading import Thread
 import re
 import time
+import os
 clientes=1
-toAll = "Bem Vindo"
+toAll = " "
+msgAntiga = " "
 lista = []
-connection = ""
-flag = 0
+socketList = []
+nickList = []
 
-def envio_mensagem():
-    global toAll
-    global connection
-    global flag
-    conn_envio = connection
-    lixo = 0
-    msgAntiga = " "
+
+
+def controle():
+    global atual
     while True:
-        if flag == 1:
-            flag = 0
-            break
-        if toAll != msgAntiga:
-            conn_envio.send(toAll)
-            msgAntiga = toAll
-        else:
-            lixo = 0
-        
+        comando = raw_input()
+        if comando == "sair()":
+            for i in range (0,atual-1):
+                socketList[i].send("163563vxdgrw56732fdrwet4")
+            time.sleep(4)
+            os._exit(1)
+            
+            
+            
 
 def conexao_cliente():
     global clientes
     global toAll
     global lista
-    global connection
-    global flag
-    flag=0
+    global socketlist
+    global atual
+    global nickList
     conn, addr = s.accept()
-    connection = conn
+    socketList.append(conn)
+    identidade = clientes
     clientes = clientes+1
     nick = conn.recv(1024)
+    while nick in nickList:
+        conn.send("Esse nick ja existe, escolha outro nick")
+        nick = conn.recv(1024)
+    nickList.append(nick)
     lista.append("<"+str(nick)+str(addr)+">")
-    Thread(target=envio_mensagem).start()
+    toAll = str(addr)+" "+str(nick)+" acabou de se conectar."
     print addr, nick, " acabou de se conectar."
     while True:
-        flag=0
         data = conn.recv(1024)
         mensagem = str(data)
         if not data:
-            flag = 1
+            atual = atual-1
+            clientes = clientes-1
             print addr, nick, "saiu do chat"
             toAll = str(addr)+str(nick)+" saiu do chat."
             time.sleep(1)
@@ -60,31 +64,44 @@ def conexao_cliente():
             end = mensagem.find(')' , start)
             nickantigo = nick
             nick = mensagem[start:end]
+            nickList.remove(nickantigo)
+            while nick in nickList:
+                conn.send("Esse nick ja existe, escolha outro nick")
+                nick = conn.recv(1024)
+            nickList.append(nick)
             toAll = str(nickantigo)+" mudou seu nickname para "+str(nick)
-       # elif mensagem[0:6]=="sair()":
-        #    flag = 1
-         #   print addr, nick, "saiu do chat"
-          #  toAll = str(addr)+str(nick)+" saiu do chat."
-           # break
+        elif mensagem[0:6]=="sair()":
+            socketList.remove(conn)
+            atual = atual-1
+            clientes = clientes-1
+            print addr, nick, "saiu do chat"
+            toAll = str(addr)+str(nick)+" saiu do chat."
+            break
         else:
             toAll = str(addr) + " " + str(nick) + " enviou: " + mensagem
         print toAll
 
 
 HOST = 'localhost'
-PORT = 7212
+PORT = 7214
 s = socket()
 s.bind((HOST, PORT))
 s.listen(9999)
 
 print "O servidor de chat esta rodando..."
 Thread(target=conexao_cliente).start()
+Thread(target=controle).start()
 atual = 1
 while True:
     if atual < clientes:
         Thread(target=conexao_cliente).start()
         atual = atual +1
     else:
+        if toAll != msgAntiga:
+            if atual-1 > 0:
+                for i in range (0,atual-1):
+                    socketList[i].send(toAll)
+                    msgAntiga = toAll
         atual = atual
 
 s.close()
